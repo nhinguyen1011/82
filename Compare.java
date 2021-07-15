8c2d550d109e055e1028b17149b4c612e97704f4
nhinguyen1011 <75206214+nhinguyen1011@users.noreply.github.com>
2021-07-15 12:00:54 +0700

    Update Compare.java

--- Compare.java
+++ Compare.java
@@ -5,51 +5,50 @@ import java.util.LinkedList;
 
 public class Compare {
     static int n = 100000;
+
     public static void main(String[] args) {
         arrayListvslinkedList();
     }
-    public static void arrayListvslinkedList(){
-        Integer[] array = new Integer[n];
+
+    public static void arrayListvslinkedList() {
         ArrayList<Integer> arrayList = new ArrayList<>();
         LinkedList<Integer> linkedList = new LinkedList<>();
 
         long start = System.nanoTime();
-
-        for (int i = 0; i <arrayList.size(); i++)
-        {
-            arrayList.get(50000);
-        }
-        System.out.println("ArrayList: "+(System.nanoTime() - start)/1000000.0 + " ms");
-
         start = System.nanoTime();
-        for (int i = 0; i <linkedList.size() ; i++)
+        for (int i = 0; i < n; i++)
         {
-            linkedList.get(50000);
+            arrayList.add(i);
         }
-        System.out.println("LinkedList: "+(System.nanoTime() - start)/1000000.0 + " ms");
-        start = System.nanoTime();
-        for (int i = 0; i <arrayList.size() ; i++)
-        {
-            arrayList.remove(50000);
-        }
-        System.out.println("ArrayList: "+(System.nanoTime() - start)/1000000.0 + " ms");
+        System.out.println("ArrayList : " + (System.nanoTime() - start)/1000000.0 + " ms");
 
         start = System.nanoTime();
-        for (int i = 0; i <linkedList.size() ; i++)
-        {
-            linkedList.remove(50000);
-        }
-        System.out.println("LinkedList: "+(System.nanoTime() - start)/1000000.0 + " ms");
-        start = System.nanoTime();
-        for (Object item : arrayList) {
-            Object x = item;
-        }
-        System.out.println("ArrayList: " + (System.nanoTime() - start)/1000000.0 + " ms");
-
-        start = System.nanoTime();
-        for (Object item : linkedList) {
-            Object x = item;
+        for (int i = 0; i < n; i++) {
+            linkedList.add(i);
         }
         System.out.println("LinkedList: " + (System.nanoTime() - start)/1000000.0 + " ms");
+
+        for (int i = 0; i < n; i++) {
+            arrayList.get(n/2);
+        }
+        System.out.println("ArrayList: " + (System.nanoTime() - start) / 1000000.0 + " ms");
+
+        start = System.nanoTime();
+        for (int i = 0; i <n; i++) {
+            linkedList.get(n/2);
+        }
+        System.out.println("LinkedList: " + (System.nanoTime() - start) / 1000000.0 + " ms");
+        start = System.nanoTime();
+        for (int i = 9999; i >=0; i--) {
+            arrayList.remove(100000/2);
+        }
+
+        System.out.println("ArrayList: " + (System.nanoTime() - start) / 1000000.0 + " ms");
+
+        start = System.nanoTime();
+        for (int i = 9999; i >=0; i--) {
+            linkedList.remove(100000/2);
+ }
+        System.out.println("LinkedList: " + (System.nanoTime() - start) / 1000000.0 + " ms");
     }
 }
